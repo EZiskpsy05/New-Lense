f633f9eeed915c7261c305f7f74d4599e5d5b812
Jimmy Chau <jimmychau999@gmail.com>
2025-12-27 19:32:08 -0500

    we got audio bababy

--- notsure/src/main.jsx
+++ notsure/src/main.jsx
@@ -1,17 +1,28 @@
 import { createRoot } from 'react-dom/client'
 import shockedbaby from './assets/Images/shockedbaby.png';
 import monopoly_money from './assets/Images/monopoly.png';
+import miau_audio from './assets/Audio/montagem_miau.m4a';
+
+function play_montagem_miau()
+{
+  new Audio(miau_audio).play()
+}
 
 createRoot(document.getElementById('root')).render(
   <div>
     <h1>Hello React!</h1>
-    <h0> I am small </h0>
+    <h0> I am wsmall </h0>
     <img src = {monopoly_money} alt = ""/>
     <img src = {shockedbaby} 
         alt = "" 
         style = {{width:'400px', height: 'auto'}}/>
-
+    
+    <button onClick = {play_montagem_miau}>
+      Press to play montagem miau
+    </button>
   </div>
+
+
 )  
 /*
 
