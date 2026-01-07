7559c9afbdf26cb5070312bb655f5dde4712d1c9
Jimmy Chau <jimmychau999@gmail.com>
2026-01-07 15:21:29 -0500

    damnn

---
+++ Code/frontend/src/Pages/home_screen.jsx
@@ -0,0 +1,8 @@
+export function home_screen(){
+    return(
+        <div>
+            <h1> home screen or something idk </h1>
+        </div>
+    )
+
+}
--- notsure/frontend/src/Pages/home_screen.jsx
+++
@@ -1,4 +0,0 @@
-export function home_screen(){
-
-
-}
